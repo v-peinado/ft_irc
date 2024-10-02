9eb1732442eb70f99ab4d069d28faa3639b95d81
v-peinado <peinado1985@gmail.com>
2024-10-02 10:26:09 +0200

    topic

---
+++ inc/command/Topic.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Topic.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpeinado <victor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 14:17:14 by vpeinado          #+#    #+#             */
+/*   Updated: 2024/10/01 14:46:21 by vpeinado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Invite_HPP
+#define Invite_HPP
+
+#include "ACommand.hpp"
+#include "Server.hpp"
+
+class Topic : public ACommand
+{
+    private:
+
+        /*** Orthodox Canonical Form ***/
+        
+        Topic(const Topic &src);
+        Topic &operator=(const Topic &src);
+        Topic();
+        
+    public:
+        
+        /*** Orthodox Canonical Form ***/
+        
+        Topic(Server &server);
+        ~Topic();
+
+        /*** MEMBER FUNCTIONS ***/
+        
+        int validArgs(std::vector<std::string> args, int fdClient);
+        void run(std::vector<std::string> args, int fdClient);  
+            
+};
